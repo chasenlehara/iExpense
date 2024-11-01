8296a752822cc5d87b8882d33430a02d8f18c089
Chasen Le Hara <apple@chasen.dev>
2024-11-01 14:52:05 -0700

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Chasen Le Hara on 11/1/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iExpenseTests/iExpenseTests.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseTests.swift
+//  iExpenseTests
+//
+//  Created by Chasen Le Hara on 11/1/24.
+//
+
+import Testing
+@testable import iExpense
+
+struct iExpenseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
