97ebcba06383c5d7c35d339afb1d65d94b892c5f
Chasen Le Hara <apple@chasen.dev>
2024-11-22 19:13:41 -0800

    Create AddView.swift

---
+++ iExpense/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Chasen Le Hara on 11/1/24.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
